20136e2c77c8c88ddd4ddfc91759e06e88a18091
zsolt270 <zsolt270000@gmail.com>
2025-03-08 00:44:49 +0100

    create tournament button styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,9 @@
     {{-- main class="container mt-5" --}}
 
     <div class="text-center">
-        <button class="btn btn-primary">Create Tournament</button>
+        <button class="btn btn-primary shadow px-5 py-2 fw-semibold fs-5 rounded-pill">Create Tournament</button>
     </div>
-    <div class="mt-5 d-flex gap-5 flex-wrap">
+    <div class="mt-5 d-flex justify-content-center justify-content-md-start gap-5 flex-wrap">
         @foreach ($tournaments as $tournament)
         <x-card name="{{$tournament['name']}}" game="{{$tournament['game']}}" date="{{$tournament['date']}}"
             country="{{$tournament['country']}}" address="{{$tournament['address']}}">
