add8124f66a51220d586f873a48337b00ed240d5
zsolt270 <zsolt270000@gmail.com>
2025-03-12 00:43:19 +0100

    Refactor detailsPage.js

--- resources/js/detailsPage.js
+++ resources/js/detailsPage.js
@@ -19,52 +19,61 @@ $(() => {
         },
     });
 
+    function sendRequest(url, method, data, successCallback, errorCallback) {
+        $.ajax({
+            url: url,
+            data: data,
+            type: method,
+            dataType: "json",
+            success: successCallback,
+            error: errorCallback,
+        });
+    }
+
     //create round
     $("#createRound").on("submit", function (e) {
         e.preventDefault();
-
-        $.ajax({
-            url: `/${tournamentId}/create-round`,
-            data: $("#createRound").serialize(),
-            type: "post",
-            dataType: "json",
-            success: function (result) {
+        sendRequest(
+            `/${tournamentId}/create-round`,
+            "post",
+            $("#createRound").serialize(),
+            function (result) {
                 const newAccordion = result.html;
                 $("#roundsAccordion").replaceWith(newAccordion);
                 $("#createRound")[0].reset();
                 $("#createError-name").text("");
                 createRoundModal.hide();
             },
-            error: function (error) {
+            function (error) {
                 $("#createError-name").text("");
 
                 $("#createError-name").text(error.responseJSON.message);
-            },
-        });
+            }
+        );
     });
 
     //update round
     $("#updateRound").on("submit", function (e) {
         e.preventDefault();
-
-        $.ajax({
-            url: `/${tournamentId}/${roundId}`,
-            data: $("#updateRound").serialize(),
-            type: "patch",
-            dataType: "json",
-            success: function (result) {
+        sendRequest(
+            `/${tournamentId}/${roundId}`,
+            "patch",
+            $("#updateRound").serialize(),
+            function (result) {
                 const newAccordion = result.html;
                 $("#roundsAccordion").replaceWith(newAccordion);
+                $(".accordion-item .collapse.show").removeClass("show");
+                $(".accordion-item #" + roundId + ".collapse").addClass("show");
                 $("#updateRound")[0].reset();
                 $("#updateError-name").text("");
                 editRoundModal.hide();
             },
-            error: function (error) {
+            function (error) {
                 $("#updateError-name").text("");
 
                 $("#updateError-name").text(error.responseJSON.message);
-            },
-        });
+            }
+        );
     });
 
     //delete round, get userslist, add users as competitors to round
@@ -74,59 +83,64 @@ $(() => {
             .parents(".accordion-item")
             .children(".accordion-collapse")
             .attr("id");
+
         //delete round
         if (
             $(e.target).is("button.btn-outline-danger") &&
             $(e.target).text() === "Delete"
         ) {
-            $.ajax({
-                url: `/${tournamentId}/${roundId}`,
-                type: "delete",
-                dataType: "json",
-                success: function (result) {
+            sendRequest(
+                `/${tournamentId}/${roundId}`,
+                "delete",
+                {},
+                function (result) {
                     const newAccordion = result.html;
                     $("#roundsAccordion").replaceWith(newAccordion);
+
                     $("#updateRound")[0].reset();
                     $("#updateError-name").text("");
                     editRoundModal.hide();
                 },
-                error: function (error) {
+                function (error) {
                     $("#updateError-name").text("");
                     $("#updateError-name").text(error.responseJSON.message);
-                },
-            });
+                }
+            );
         }
+
         //get userslist
         if ($(e.target).is("button[data-bs-target='#addCompetitorsModal']")) {
-            $.ajax({
-                url: `/${tournamentId}/${roundId}/competitors`,
-                type: "get",
-                dataType: "json",
-                success: function (result) {
+            sendRequest(
+                `/${tournamentId}/${roundId}/competitors`,
+                "get",
+                {},
+                function (result) {
                     console.log(result);
-                    // $("#roundsAccordion").replaceWith(result.accordion);
                     $("#usersContainer").replaceWith(result.usersList);
                 },
-                error: function (error) {
+                function (error) {
                     console.log(error);
-                },
-            });
+                }
+            );
         }
+
         //delete competitor from round
         if ($(e.target).is("button[data-uid]")) {
-            $.ajax({
-                url: `/${tournamentId}/${roundId}/${$(e.target).attr(
-                    "data-uid"
-                )}`,
-                type: "delete",
-                dataType: "json",
-                success: function (result) {
+            sendRequest(
+                `/${tournamentId}/${roundId}/${$(e.target).attr("data-uid")}`,
+                "delete",
+                {},
+                function (result) {
                     $("#roundsAccordion").replaceWith(result.html);
+                    $(".accordion-item .collapse.show").removeClass("show");
+                    $(".accordion-item #" + roundId + ".collapse").addClass(
+                        "show"
+                    );
                 },
-                error: function (error) {
+                function (error) {
                     console.log(error);
-                },
-            });
+                }
+            );
         }
     });
 
@@ -137,18 +151,17 @@ $(() => {
         clearTimeout(deboucedRequest);
 
         deboucedRequest = setTimeout(() => {
-            $.ajax({
-                url: `/${tournamentId}/${roundId}/search`,
-                data: { query },
-                type: "get",
-                dataType: "json",
-                success: function (result) {
+            sendRequest(
+                `/${tournamentId}/${roundId}/search`,
+                "get",
+                { query },
+                function (result) {
                     $("#usersContainer").replaceWith(result.usersList);
                 },
-                error: function (error) {
+                function (error) {
                     console.log(error);
-                },
-            });
+                }
+            );
         }, 1000);
     });
 
@@ -161,20 +174,24 @@ $(() => {
             $(e.target).find("button").text("✓");
             $(e.target).find("p").css("font-weight", "500");
 
-            $.ajax({
-                url: `/${tournamentId}/${roundId}/${
+            sendRequest(
+                `/${tournamentId}/${roundId}/${
                     $(e.target).attr("id").split("-")[1]
                 }`,
-                type: "post",
-                dataType: "json",
-                success: function (result) {
+                "post",
+                {},
+                function (result) {
                     $("#roundsAccordion").replaceWith(result.accordion);
                     $("#usersContainer").replaceWith(result.usersList);
+                    $(".accordion-item .collapse.show").removeClass("show");
+                    $(".accordion-item #" + roundId + ".collapse").addClass(
+                        "show"
+                    );
                 },
-                error: function (error) {
+                function (error) {
                     console.log(error);
-                },
-            });
+                }
+            );
         });
 
     // pagination
@@ -182,17 +199,17 @@ $(() => {
         .parent()
         .on("click", ".pagination a", function (e) {
             e.preventDefault();
-            $.ajax({
-                url:
-                    `/${tournamentId}/${roundId}/competitors?page=` +
+            sendRequest(
+                `/${tournamentId}/${roundId}/competitors?page=` +
                     $(this).attr("href").split("page=")[1],
-                method: "GET",
-                success: function (result) {
+                "GET",
+                {},
+                function (result) {
                     $("#usersContainer").replaceWith(result.usersList);
                 },
-                error: function (error) {
+                function (error) {
                     console.log(error);
-                },
-            });
+                }
+            );
         });
 });
